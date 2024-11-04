c44c6091868c683c657ba7bec9ecf0ab2421cbfa
areebasaleem456 <areebasaleem771@gmail.com>
2024-11-04 14:17:24 +0500

    Add files via upload

---
+++ milestone1/main.js
@@ -0,0 +1,10 @@
+var toggle = document.getElementById("headings");
+var skills = document.getElementById("headings");
+toggle.addEventListener("click", function () {
+    if (skills.style.display === "none") {
+        skills.style.display = "block";
+    }
+    else {
+        skills.style.display = "none";
+    }
+});
